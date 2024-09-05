71250bbe5de4209f87e4f82909169eeb8d7759ac
Sohelrana2815 <sohel152302@gmail.com>
2024-09-05 12:54:07 +0600

    navbar

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,10 +1,19 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Shared/Navbar/Navbar";
+import Footer from "../Shared/Footer/Footer";
 
+// 63AB45
 const Root = () => {
   return (
-    <div>
-      <Outlet />
-    </div>
+    <>
+      <div className="max-w-screen-xl mx-auto min-h-screen flex flex-col mt-10">
+        <Navbar />
+        <div className="flex-grow">
+          <Outlet />
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 };
 
